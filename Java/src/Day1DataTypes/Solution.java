e5cd05926c876d5c7a82d3b68251165610606866
Julieta Estrada <99842470+Julieta-Estrada@users.noreply.github.com>
2022-07-30 16:10:16 -0500

    Feat: 3 days HackerRank

---
+++ Java/src/Day1DataTypes/Solution.java
@@ -0,0 +1,31 @@
+package Day1DataTypes;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		/* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+
+        Scanner sc = new Scanner(System.in);
+        int i2 = sc.nextInt();
+        double d2 = sc.nextDouble();
+        sc.nextLine();
+        String s2 = sc.nextLine();
+    
+        System.out.println(i + i2 );
+        System.out.println(d2 + d);
+        System.out.println("HackerRank " + s2 );
+
+	}
+
+}
---
+++ Python/Day1DataType.py
@@ -0,0 +1,15 @@
+# Declare second integer, double, and String variables.
+i2 = int (input());
+d2 = float (input());
+s2 = input();
+# Read and save an integer, double, and String to your variables.
+i=0
+d=0
+s=0
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+# Print the sum of the double variables on a new line.
+print(d + d2);
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print (s + s2)
